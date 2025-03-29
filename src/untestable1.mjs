3bbb686d4d63af68e1528734e3479883f6e65417
HHKProgrammer <helin@DESKTOP-AE9GKSP>
2025-03-29 13:30:39 +0200

    refactored unstable

--- src/untestable1.mjs
+++ src/untestable1.mjs
@@ -2,7 +2,7 @@ const millisPerDay = 24 * 60 * 60 * 1000;
 
 export function daysUntilChristmas(today = new Date()) {
   const year = today.getFullYear(); //hard coded
-  const christmasDay = new Date(year, 12, 25); //december 25th but
+  const christmasDay = new Date(year, 11, 25); //december 25th but
  //const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
   //const christmasDay = new Date(now.getFullYear(), 12 - 1, 25);
   if (today > christmasDay) {
--- test/untestable1.test.mjs
+++ test/untestable1.test.mjs
@@ -3,8 +3,10 @@ import { expect } from "chai";
 import { daysUntilChristmas } from "../src/untestable1.mjs";
 
 describe("Untestable 1: days until Christmas", () => {
-  test("todo", () => {
-    // TODO: write proper tests
-    expect(daysUntilChristmas()).to.be.a("number");
+  test("Calculates days correctly when before Christmas", () => {
+    const mockDate = new Date("2025-12-20");
+    const result = daysUntilChristmas(mockDate);
+    expect(result).to.equal(5); // 5 days until cchristmas
+   //expect(daysUntilChristmas()).to.be.a("number");
   });
 });
