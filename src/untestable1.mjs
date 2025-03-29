4ac9d214effbe6e026276706a769a1a50f3a3107
HHKProgrammer <helin@DESKTOP-AE9GKSP>
2025-03-29 14:02:32 +0200

    second test for unstable

--- src/untestable1.mjs
+++ src/untestable1.mjs
@@ -5,6 +5,9 @@ export function daysUntilChristmas(today = new Date()) {
   const christmasDay = new Date(year, 11, 25); //december 25th but
  //const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
   //const christmasDay = new Date(now.getFullYear(), 12 - 1, 25);
+  //  both dates to midnight (00:00:00) for  comparison
+  today.setHours(0, 0, 0, 0);
+  christmasDay.setHours(0, 0, 0, 0);
   if (today > christmasDay) {
     christmasDay.setFullYear(year + 1);
   }
--- test/untestable1.test.mjs
+++ test/untestable1.test.mjs
@@ -8,5 +8,12 @@ describe("Untestable 1: days until Christmas", () => {
     const result = daysUntilChristmas(mockDate);
     expect(result).to.equal(5); // 5 days until cchristmas
    //expect(daysUntilChristmas()).to.be.a("number");
+
+  });
+
+  test("Calculates days correctly when it is Christmas Day", () => {
+    const mockDate = new Date("2025-12-25"); // today christmas day
+    const result = daysUntilChristmas(mockDate);
+    expect(result).to.equal(0); // today so 0 days left
   });
 });
