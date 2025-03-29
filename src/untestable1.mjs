ec75f974358735e3fb972916bfed98eee7316bb9
HHKProgrammer <helin@DESKTOP-AE9GKSP>
2025-03-29 13:16:26 +0200

    refactored unstable 1

--- src/untestable1.mjs
+++ src/untestable1.mjs
@@ -1,12 +1,13 @@
 const millisPerDay = 24 * 60 * 60 * 1000;
 
-export function daysUntilChristmas() {
-  const now = new Date();
-  const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-  const christmasDay = new Date(now.getFullYear(), 12 - 1, 25);
-  if (today.getTime() > christmasDay.getTime()) {
-    christmasDay.setFullYear(new Date().getFullYear() + 1);
+export function daysUntilChristmas(today = new Date()) {
+  const year = today.getFullYear(); //hard coded
+  const christmasDay = new Date(year, 12, 25); //december 25th but
+ //const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+  //const christmasDay = new Date(now.getFullYear(), 12 - 1, 25);
+  if (today > christmasDay) {
+    christmasDay.setFullYear(year + 1);
   }
-  const diffMillis = christmasDay.getTime() - today.getTime();
-  return Math.floor(diffMillis / millisPerDay);
+  const diff = christmasDay - today;
+  return Math.floor(diff / millisPerDay);
 }
